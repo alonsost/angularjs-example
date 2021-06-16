b3363f04dbc0c3732bb3b23814c262d038c527cb
alonsosjwb <alonsosalcido@canplast.com.mx>
2021-06-16 13:31:06 -0600

    icreating first controller and first variable on scope to show on the client html side

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-/*global angular */
+/*global angular  https://developer.mozilla.org/es/docs/Glossary/IIFE*/
 (function () {
+    const gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description:'Some gems have hidden qualities beyond their luster, beyond their shine.. Dodecahedron is one of those gems.'
+    }
 
-	app = angular.module("gemStore", []);
+	app = angular.module("gemStore", [])
+
+    app.controller('StoreController', function(){
+        this.product = gem
+    })
 
 }());
