85ed21a6c01a361230b07f97106818b4e6cf799c
alonsosjwb <alonsosalcido@canplast.com.mx>
2021-06-20 21:33:44 -0600

    use ng-hide on controller

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
         name: 'Dodecahedron',
         price: 2.95,
         description:'Some gems have hidden qualities beyond their luster, beyond their shine.. Dodecahedron is one of those gems.',
-        canPurchase: false
+        canPurchase: false,
+        soldOut: true
     }
 
 	app = angular.module("gemStore", [])
