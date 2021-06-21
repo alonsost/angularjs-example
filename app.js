0bee602b266a813a70529f0cb774f34458602f9a
alonsosjwb <alonsosalcido@canplast.com.mx>
2021-06-20 21:36:45 -0600

    use arrays first element on gems and list

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 /*global angular  https://developer.mozilla.org/es/docs/Glossary/IIFE*/
 (function () {
-    const gem = {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description:'Some gems have hidden qualities beyond their luster, beyond their shine.. Dodecahedron is one of those gems.',
-        canPurchase: false,
-        soldOut: true
-    }
+    const gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description:'Some gems have hidden qualities beyond their luster, beyond their      shine.. Dodecahedron is one of those gems.',
+            canPurchase: false,
+            soldOut: false
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description:'Some gems have hidden qualities beyond their luster, beyond their shine.. Pentagonal is one of those gems.',
+            canPurchase: false,
+            soldOut: false
+        }
+    ]
 
 	app = angular.module("gemStore", [])
 
     app.controller('StoreController', function(){
-        this.product = gem
+        this.products = gems
     })
 
 }());
